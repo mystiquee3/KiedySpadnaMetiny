0dfdd0c2c7ec562457c09ec891c5066378eaebe6
mystiquee3 <krystian.lubanski@wp.pl>
2025-02-27 09:19:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const startDate = new Date("2025-02-20T23:02:45");
+    const startDate = new Date("2025-02-27T10:08:35");
     const interval = (1 * 60 * 60 * 1000) + (2 * 60 * 1000) + (30 * 1000);
 
     let nextDate = calculateNextDate();
